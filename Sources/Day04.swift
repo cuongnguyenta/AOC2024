1fea52da488bf40b75f2b163b8731038a9916583
Cường Nguyễn Tạ <cuongnt04230@gmail.com>
2024-12-06 17:55:17 +0700

    Day 5 part 1

---
+++ Sources/Day04.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+struct Rule: Hashable {
+    let page1: Int
+    let page2: Int
+}
+
+struct Day04: AdventDay {
+    private let rules: Set<Rule>
+    private let updates: [[Int]]
+    
+    init(data: String) {
+        let lines = data.components(separatedBy: .newlines)
+        let separatorIndex = lines.firstIndex(of: "") ?? lines.count
+        
+        let rulesStrings = lines[..<separatorIndex]
+        let updatesStrings = lines[(separatorIndex + 1)...]
+        
+        self.rules = Set(rulesStrings.map { ruleString -> Rule in
+            let numbers = ruleString.components(separatedBy: "|").compactMap { Int($0) }
+            return Rule(page1: numbers[0], page2: numbers[1])
+        })
+        
+        self.updates = updatesStrings.map { update in
+            update.components(separatedBy: ",").compactMap { Int($0) }
+        }
+    }
+    
+    private func updateOk(_ update: [Int]) -> Bool {
+        update
+            .combinations(ofCount: 2)
+            .map { Rule(page1: $0[0], page2: $0[1]) }
+            .allSatisfy { rules.contains($0) }
+    }
+    func part1() -> Int {
+        updates
+            .filter { updateOk($0) }
+            .map { $0.median() }
+            .reduce(0, +)
+    }
+}
+
+extension Array where Element == Int {
+    func median() -> Int {
+        guard !isEmpty else { return 0 }
+        let middleIndex = count / 2
+        return self[middleIndex]
+    }
+}
